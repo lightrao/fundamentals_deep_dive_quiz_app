b0aa6ad3f555a3ecc1d9e4c352669a451d31b7d5
lightrao <lightrao@hotmail.com>
2024-08-17 21:04:44 +0800

    Adding a Stateful Widget

--- lib/main.dart
+++ lib/main.dart
@@ -1,19 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:fundamentals_deep_dive_quiz_app/start_screen.dart';
+import 'package:fundamentals_deep_dive_quiz_app/quiz.dart';
 
 void main() {
-  runApp(MaterialApp(
-    home: Scaffold(
-      body: Container(
-        decoration: const BoxDecoration(
-          gradient: LinearGradient(
-            colors: [Colors.deepPurple, Colors.lightBlue],
-            begin: Alignment.topLeft,
-            end: Alignment.bottomRight,
-          ),
-        ),
-        child: const StartScreen(),
-      ),
-    ),
-  ));
+  runApp(const Quiz());
 }
