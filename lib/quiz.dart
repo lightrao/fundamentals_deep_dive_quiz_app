3f4669854d46a2e8df7b590863557fcf332aba5a
lightrao <lightrao@hotmail.com>
2024-08-18 11:03:22 +0800

    Understanding "if" Statements

--- lib/quiz.dart
+++ lib/quiz.dart
@@ -23,6 +23,11 @@ class _QuizState extends State<Quiz> {
 
   @override
   Widget build(BuildContext context) {
+    Widget screenWidget = StartScreen(switchScreen);
+    if (activeScreen == 'questions-screen') {
+      screenWidget = const QuestionsScreen();
+    }
+
     return MaterialApp(
       home: Scaffold(
         body: Container(
@@ -33,9 +38,7 @@ class _QuizState extends State<Quiz> {
               end: Alignment.bottomRight,
             ),
           ),
-          child: activeScreen == 'start-screen'
-              ? StartScreen(switchScreen)
-              : const QuestionsScreen(),
+          child: screenWidget,
         ),
       ),
     );
