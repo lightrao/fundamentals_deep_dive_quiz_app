df96a14ab2a8e7149b2025338f2e79f477f5a8bb
lightrao <lightrao@hotmail.com>
2024-08-20 08:08:54 +0800

    Introducing Maps & "for" Loops

--- lib/results_screen.dart
+++ lib/results_screen.dart
@@ -1,9 +1,28 @@
 import 'package:flutter/material.dart';
 
+import 'package:fundamentals_deep_dive_quiz_app/data/questions.dart';
+
 class ResultsScreen extends StatelessWidget {
   const ResultsScreen({super.key, required this.chosenAnswers});
 
   final List<String> chosenAnswers;
+
+  List<Map<String, Object>> getSummaryData() {
+    final List<Map<String, Object>> summary = [];
+
+    for (var i = 0; i < chosenAnswers.length; i++) {
+      summary.add(
+        {
+          'question_index': i,
+          'question': questions[i].text,
+          'correct_anwser': questions[i].answers[0],
+          'user_anser': chosenAnswers[i],
+        },
+      );
+    }
+
+    return summary;
+  }
 
   @override
   Widget build(BuildContext context) {
